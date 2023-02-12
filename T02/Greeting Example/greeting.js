3bb3ce3c51685c07bbfd8ab9d35c25be3c5a0b18
cek <christinakime@gmail.com>
2023-02-12 19:36:49 +0000

    Task 2

---
+++ T02/Greeting Example/greeting.js
@@ -0,0 +1,7 @@
+// Code here for Compulsory Task 1
+let userName = prompt("What is your name?")
+console.log(userName)
+let userAge = prompt("What is your age?")
+console.log(userAge)
+
+console.log("\nIt's nice to meet you!")
---
+++ T02/Shopping Example/shopping.js
@@ -0,0 +1,8 @@
+// Code here for Compulsory Task 2
+let item1 = prompt("What is your first item?")
+let item2 = prompt("What is your second item?")
+let item3 = prompt("What is your third item?")
+
+let shoppingList = [item1, item2, item3]
+
+console.log("My Shopping List:\n"+shoppingList[0]+"\n"+shoppingList[1]+"\n"+shoppingList[2])
